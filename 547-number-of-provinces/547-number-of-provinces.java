d1135658e6dd2057f573e94e1ea9e401f437bd97
Joe <yussefislamanwer@gmail.com>
2022-03-19 19:04:03 +0200

    Time: 0 ms (100.00%), Space: 42.8 MB (93.56%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -0,0 +1,26 @@
+class Solution {
+    private void dfs(int node, int[][] isConnected, boolean [] visited){
+       visited[node - 1] = true; 
+        // i = 2.
+        //
+       for(int i = 1; i <= isConnected[node - 1].length ; i++){
+           if(isConnected[node - 1][i - 1] == 1 && !visited[i - 1]){
+               dfs(i, isConnected, visited);
+           }
+       }
+    }
+    
+    public int findCircleNum(int[][] isConnected) {
+        boolean visited[] = new boolean[isConnected[0].length];
+        int provincesNum = 0;
+        
+        for(int i = 1; i <= isConnected[0].length ; i++){
+            if(!visited[i - 1]){
+                provincesNum++;
+                dfs(i, isConnected, visited);
+            }
+        }
+        
+        return provincesNum;
+    }
+}
