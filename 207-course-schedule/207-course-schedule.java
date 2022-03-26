cd74956e4127f0692ced6cfc4db23157748a761e
Joe <yussefislamanwer@gmail.com>
2022-03-26 19:07:41 +0200

    Time: 13 ms (31.31%), Space: 49.6 MB (22.83%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,10 +1,13 @@
 class Solution {
     
     
-     public static boolean canFinish(int numCourses, int[][] prerequisites) {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    // Create edges using a hashmap, so the course key is dependent on the course value.
     HashMap<Integer, List<Integer>> prereqMap = new HashMap<>();
-    Set<Integer> visitedSet = new HashSet<>();
+    // VisitingSet is used to track cycles.
+    Set<Integer> visitingSet = new HashSet<>();
 
+    // Filling the map with empty list or adding a node connection to the list of edges.
     for (int i = 0; i < prerequisites.length; i++) {
       List<Integer> requiredCourses = prereqMap.getOrDefault(prerequisites[i][0],
           new ArrayList<Integer>());
@@ -12,8 +15,9 @@ class Solution {
       prereqMap.put(prerequisites[i][0], requiredCourses);
     }
 
+    // Executing DFS on each course.
     for (int i = 0; i < numCourses; i++) {
-      if(!dfs(i, prereqMap, visitedSet)) return false;
+      if(!dfs(i, prereqMap, visitingSet)) return false;
     }
 
     
@@ -21,24 +25,31 @@ class Solution {
     return true;
   }
 
-  private static boolean dfs(int node, HashMap<Integer, List<Integer>> prereqMap, Set<Integer> visitedSet) {
+  private boolean dfs(int node, HashMap<Integer, List<Integer>> prereqMap, Set<Integer> visitingSet) {
     List<Integer> neighbourCourses = prereqMap.getOrDefault(node, new ArrayList<Integer>());
 
-    if (visitedSet.contains(node) )
+    // If we're visiting a node that's already visited, then we've come into a cycle.
+    if (visitingSet.contains(node) )
       return false;
     
+    // If the prerequisite courses don't existed to a specific course, then there's no cycle. 
     if(neighbourCourses.isEmpty())
       return true;
 
-    visitedSet.add(node);
+    // Since we're currently visiting the node, mark it as a visiting node and start
+    // visiting other nodes.
+    visitingSet.add(node);
 
+    // If each node result in a cycle, return false.
     for (int neighbourCourse : neighbourCourses) {
-      if(!dfs(neighbourCourse, prereqMap, visitedSet)){
+      if(!dfs(neighbourCourse, prereqMap, visitingSet)){
         return false;
       }
     }
+    // Otherwise make the prerequisite courses (neighbour nodes) empty for that node
     prereqMap.put(node, new ArrayList<>());
-    visitedSet.remove(node);
+    // Remove the node from the set since we're done with it and it's already visited.
+    visitingSet.remove(node);
     return true;
 
   }
