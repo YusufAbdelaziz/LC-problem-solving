17941cd2882ba597300c1d34a732e54c8cfbf501
Joe <yussefislamanwer@gmail.com>
2022-07-13 19:57:51 +0200

    Time: 6 ms (63.45%), Space: 45 MB (35.74%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,8 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
+        // Convert the first string to a list of characters.
         var tempArrayS = s.toCharArray();
+        // Sort that list of characters.
         Arrays.sort(tempArrayS);
+        // Construct the list of characters into a string.
         String sortedS = new String(tempArrayS);
         
         var tempArrayT = t.toCharArray();
