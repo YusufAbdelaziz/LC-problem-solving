456b5d63a432424f1b859055881fd942a754c482
Joe <yussefislamanwer@gmail.com>
2022-08-30 21:24:19 +0200

    Time: 22 ms (30.30%), Space: 71.2 MB (25.51%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // O(n) solution with O(n) space.
-        Set<Integer> set = new HashSet<>();
+       // Time complexity : O(n)
+       // Space Complexity : O(n)
+        Set<Integer> hashSet = new HashSet<>();
         
         for(int num : nums){
-            
-            if(set.contains(num)){
-                return true;
-            }
-            
-            set.add(num);
+            if(hashSet.contains(num)) return true;
+            hashSet.add(num);
         }
         
         return false;
