a00c74dc6d7e6875375a32b28c6f209effdcec74
Joe <yussefislamanwer@gmail.com>
2022-08-30 22:04:18 +0200

    Update 1-two-sum.java

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,22 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], i);
-        } 
-        
-        int[] sol = new int[2];
-        
-        for(int i = 0 ; i < nums.length ; i ++){
-            var secondNum = target - nums[i];
-            var secondNumIndex = map.get(secondNum);
-            if(secondNumIndex != null && secondNumIndex != i){
-                sol[0] = i;
-                sol[1] = secondNumIndex;
-                break;
-            }
-        }
-        
+        var sol = new int[2];
+        for(int i = 0 ; i < nums.length ; i++){
+            int diff = target - nums[i];
+           if(map.containsKey(diff)){
+               sol[0] = i;
+               sol[1] = map.get(diff);
+               return arr;
+           }
+          map.put(nums[i], i);
+       }
         return sol;
     }
 }
