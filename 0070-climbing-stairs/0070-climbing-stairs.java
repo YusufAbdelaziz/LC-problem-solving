62f20a9732d2e9cfad9c71195223f756819bc0c4
Joe <yussefislamanwer@gmail.com>
2023-02-15 19:29:33 +0200

    Time: 0 ms (100.00%), Space: 39.1 MB (62.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,8 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
         /// These two variables represent the last two possible steps.
-        int one = 1, two = 1;
-        for(int i = n - 2 ; i >= 0; i --){
+        int one = 1, two = 0;
+        for(int i = n - 1 ; i >= 0; i --){
             int temp = one;
             one += two;
             two = temp;
