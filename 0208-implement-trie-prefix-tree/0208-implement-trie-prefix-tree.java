c4b704ae71caf5c6dc0daafa8118d4b2a359fdb5
Joe <yussefislamanwer@gmail.com>
2023-08-07 21:15:55 +0300

    Time: 65 ms (8.60%), Space: 54.2 MB (94.12%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,105 @@
+public class Trie {
+  private final Node root;
+
+  private class Node {
+    private boolean isKey;
+    private final HashMap<Character, Node> children = new HashMap<>();
+
+    public Node(boolean isKey) {
+      this.isKey = isKey;
+    }
+
+    public void setKey(boolean isKey) {
+      this.isKey = isKey;
+    }
+
+  }
+
+  public Trie() {
+    root = new Node(false);
+  }
+
+  public void insert(String word) {
+    var currentNode = root;
+    for (int i = 0; i < word.length(); i++) {
+      if (currentNode.children.containsKey(word.charAt(i))) {
+        if (i == word.length() - 1)
+          currentNode.children.get(word.charAt(i)).setKey(true);
+        currentNode = currentNode.children.get(word.charAt(i));
+      } else {
+        currentNode.children.put(word.charAt(i), i == word.length() - 1 ? new Node(true) : new Node(false));
+        currentNode = currentNode.children.get(word.charAt(i));
+      }
+    }
+  }
+
+  public boolean search(String word) {
+    var currentNode = root;
+    for (int i = 0; i < word.length(); i++) {
+      if (currentNode.children.containsKey(word.charAt(i))) {
+        currentNode = currentNode.children.get(word.charAt(i));
+      } else {
+        return false;
+      }
+    }
+
+    return currentNode.isKey;
+  }
+
+  public boolean startsWith(String prefix) {
+    var list = keysWithPrefix(prefix);
+
+    return list != null && keysWithPrefix(prefix).size() != 0;
+  }
+
+  /**
+   * Retrieves all keys starting with a prefix.
+   * 
+   * Note that this seem redundant for startWith method as you only need to check
+   * if at least one key exists but I made it for the sake of a complete
+   * implementation.
+   * 
+   * @param prefix
+   * @return list of all keys starting with a given prefix.
+   * 
+   */
+  private List<String> keysWithPrefix(String prefix) {
+    Node lastCharNode = findPrefixNode(prefix);
+    if (lastCharNode == null)
+      return null;
+    List<String> results = new ArrayList<>();
+    for (Character c : root.children.keySet()) {
+      collectHelper(c.toString(), results, root.children.get(c));
+    }
+
+    return results;
+  }
+
+  private Node findPrefixNode(String prefix) {
+    var currentNode = root;
+    for (int i = 0; i < prefix.length(); i++) {
+      if (currentNode.children.containsKey(prefix.charAt(i))) {
+        currentNode = currentNode.children.get(prefix.charAt(i));
+      } else
+        return null;
+    }
+    return currentNode;
+  }
+
+  private void collectHelper(String s, List<String> result, Node n) {
+    if (n.isKey)
+      result.add(s);
+    if(result.size() > 0) return;
+    for (Character c : n.children.keySet()) {
+      collectHelper(s + c, result, n.children.get(c));
+    }
+  }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
