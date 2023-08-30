0e28bff041f3c38a2e16c1294aef8e8bdd1858de
Joe <yussefislamanwer@gmail.com>
2023-08-30 23:31:20 +0300

    Time: 2 ms (99.21%), Space: 43.8 MB (31.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,27 +4,43 @@ class Solution {
         // HashMap approach -> Time --> O(N), space --> O(s.length() + t.length())
         
         // A map for each character occurrences.
-        HashMap<Character, Integer> map = new HashMap<>();
+//         HashMap<Character, Integer> map = new HashMap<>();
         
-        for(char c : s.toCharArray()){
-            int count = map.getOrDefault(c, 0);
-            map.put(c, count + 1);
-        }
+//         for(char c : s.toCharArray()){
+//             int count = map.getOrDefault(c, 0);
+//             map.put(c, count + 1);
+//         }
         
-         for(char c : t.toCharArray()){
-            int count = map.getOrDefault(c, 0);
-            if(count == 0) return false;
-            map.put(c, count - 1);
+//          for(char c : t.toCharArray()){
+//             int count = map.getOrDefault(c, 0);
+//             if(count == 0) return false;
+//             map.put(c, count - 1);
 
+//         }
+        
+//         // Check if each character has removed.
+        
+//         for(int occurrence : map.values()){
+//             if(occurrence != 0) return false;
+//         }
+        
+//         return true;
+    
+//     }
+        int[] charOccurrences = new int[26];
+        
+        for(char c : s.toCharArray()) {
+            charOccurrences[c - 'a'] += 1;
         }
         
-        // Check if each character has removed.
+        for(char c : t.toCharArray()) {
+            charOccurrences[c - 'a'] -= 1;
+        }
         
-        for(int occurrence : map.values()){
-            if(occurrence != 0) return false;
+        for(int occurrences : charOccurrences) {
+            if(occurrences != 0) return false;
         }
         
         return true;
-        
     }
 }
