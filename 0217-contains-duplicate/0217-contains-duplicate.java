da3b4ea806d426f474aedff0c1fe3b9ef9fa4c6b
Joe <yussefislamanwer@gmail.com>
2023-08-30 05:16:28 +0300

    Time: 18 ms (37.36%), Space: 54.9 MB (74.05%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,7 +2,8 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         // HashMap -> Time --> O(N), Space --> O(N).
         // Sorting -> Time --> O(NlogN), Space --> Actually it depends upon the algorithm used
-        // but in case of using auxillary space it would be O(N).
+        // but in case of using auxillary space it would be O(logN) as recursive algorithms needs
+        // O(LogN) space.
         HashMap<Integer, Integer> map = new HashMap<>();
         
         for(int num : nums){
