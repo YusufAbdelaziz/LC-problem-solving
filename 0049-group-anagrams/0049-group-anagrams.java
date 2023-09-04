13eca3e086793d0e0f7dd1c982d7f1bbaef9bb1f
Joe <yussefislamanwer@gmail.com>
2023-09-04 12:03:17 +0300

    Time: 6 ms (83.14%), Space: 47 MB (56.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,9 +1,9 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<List<String>>();
-        HashMap<String, List<String>> map = new HashMap<>();
-        if(strs.length == 0) return result;
         
+        HashMap<String, List<String>> map = new HashMap<>();
+        if(strs.length == 0 || strs == null) return Collections.emptyList();
+        // Time -> O(m*nlogn) where m is the number of strings and n is the length of each string.
         for(String s : strs) {
             char[] chars = s.toCharArray();
             Arrays.sort(chars);
@@ -18,10 +18,7 @@ class Solution {
             }
         }
         
-        for(List<String> arr : map.values()){
-            result.add(arr);
-        }
         
-        return result;
+        return new ArrayList<>(map.values());
     }
 }
