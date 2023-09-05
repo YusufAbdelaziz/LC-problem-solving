5d6b286b3215949d136901de325d1b8f411f690c
Joe <yussefislamanwer@gmail.com>
2023-09-05 16:10:47 +0300

    Time: 8 ms (98.81%), Space: 47.5 MB (64.70%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,23 +2,55 @@ class Solution {
      public int[] topKFrequent(int[] nums, int k) {
       if (nums.length == 0 || nums == null || nums.length == 1)
         return nums;
+      // Time -> O(K log n) where K is the number of pops we make for the max heap.
+      // Space -> O(N) where N is the number of pairs that store the number and its
+      // repetitions.
+      // HashMap<Integer, Integer> map = new HashMap<>();
+      // LinkedList<Integer> result = new LinkedList<>();
+      // for (int num : nums) {
+      // map.put(num, map.getOrDefault(num, 0) + 1);
+      // }
+
+      // PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b)
+      // -> b.getValue() - a.getValue());
+
+      // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      // heap.add(entry);
+      // }
+
+      // for (int i = 0; i < k; i++) {
+      // result.add(heap.poll().getKey());
+      // }
+
+      // return result.stream().mapToInt(Integer::intValue).toArray();
 
       HashMap<Integer, Integer> map = new HashMap<>();
-      LinkedList<Integer> result = new LinkedList<>();
+      int[] result = new int[k];
       for (int num : nums) {
         map.put(num, map.getOrDefault(num, 0) + 1);
       }
 
-      PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+      ArrayList<Integer>[] count = new ArrayList[nums.length];
+      for (int i = 0; i < count.length; i++) {
+        count[i] = new ArrayList<Integer>();
+      }
 
       for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-        heap.add(entry);
+        var valuesArr = count[entry.getValue() - 1];
+        valuesArr.add(entry.getKey());
       }
 
-      for (int i = 0; i < k; i++) {
-        result.add(heap.poll().getKey());
+      int j = k - 1;
+
+      for (int i = count.length - 1; i >= 0 && j >= 0; i--) {
+        if (count[i].size() > 0) {
+          for (int num : count[i]) {
+            result[j] = num;
+            j--;
+          }
+        }
       }
 
-      return result.stream().mapToInt(Integer::intValue).toArray();
+      return result;
     }
 }
