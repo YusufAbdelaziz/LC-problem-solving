46009344bddf058c2715756b94b596c03f7bf79a
Joe <yussefislamanwer@gmail.com>
2023-09-11 00:36:35 +0300

    Time: 4 ms (87.51%), Space: 56.4 MB (69.52%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -14,15 +14,33 @@ class Solution {
         // }
         // return sol;
         
-        int[] temp = new int[nums.length];
-        for(int num : nums) {
-            temp[num - 1] += 1;
-        }
+//         // Time -> O(N).
+//         // Space -> O(N).
+//         // Uses an array which is a little bit faster but same space complexity as the first approach.
+//         int[] temp = new int[nums.length];
+//         for(int num : nums) {
+//             temp[num - 1] += 1;
+//         }
         
-        for(int i = 0; i < nums.length; i++) {
-            if(temp[i] >= 2) return i + 1;
-        }
+//         for(int i = 0; i < nums.length; i++) {
+//             if(temp[i] >= 2) return i + 1;
+//         }
         
-        return 0;
+       int tortoise = nums[0];
+       int hare = nums[0];
+       
+       do {
+           tortoise = nums[tortoise];
+           hare = nums[nums[hare]];
+       } while (tortoise != hare);
+       
+       tortoise = nums[0];
+       
+       while(tortoise != hare) {
+           tortoise = nums[tortoise];
+           hare = nums[hare];
+       }
+        
+       return tortoise;
     }
 }
