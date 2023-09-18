c2204b5b9c11397e47f72e1fdf2a200bdcce1d4a
Joe <yussefislamanwer@gmail.com>
2023-09-18 23:28:52 +0300

    Time: 7 ms (41.35%), Space: 43.2 MB (63.58%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,50 @@
+class Solution {
+   enum Operation{
+       ADD,
+       SUB,
+       DIV,
+       MUL
+    }
+    public int evalRPN(String[] tokens) {
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(String token : tokens) {
+            if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                int secOp = stack.pop();
+                int firstOp = stack.pop();
+                stack.push(makeOperation(token, firstOp, secOp));
+                continue;
+            }
+            
+            stack.push(Integer.parseInt(token));
+        }
+        
+        return stack.pop();
+    }
+    
+    private int makeOperation(String token, int firstOp, int secondOp) {
+        Operation op = Operation.ADD;
+        switch(token) {
+            case "/": 
+                op = Operation.DIV;
+                break;
+            case "+": 
+                op = Operation.ADD;
+                break;
+            case "-": 
+                op = Operation.SUB;
+                break;
+            case "*": 
+                op = Operation.MUL;
+                break;
+        }
+        
+        switch(op) {
+            case ADD: return firstOp + secondOp;
+            case SUB: return firstOp - secondOp; 
+            case DIV: return firstOp / secondOp; 
+            default: return firstOp * secondOp; 
+        }
+    }
+}
