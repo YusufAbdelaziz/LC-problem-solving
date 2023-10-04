28ebebe3a57479ef2296f5963e6ac7df9b493c0e
Joe <yussefislamanwer@gmail.com>
2023-10-05 00:23:51 +0300

    Time: 0 ms (100.00%), Space: 41.7 MB (18.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -13,34 +13,31 @@ class Solution {
       if(list1 == null && list2 == null) return null;
       if(list1 == null && list2 != null) return list2;
       if(list2 == null && list1 != null) return list1;
-    /// Result head is the first element in the LL.
-    /// Result tail is the node that is moving to add more elements.
-    ListNode resultHead = null, resultTail = null;
-
-    while(list1 != null | list2 != null) {
-        ListNode smallestNode = null;
-        /// If list 2 is empty or list 1 is not empty and list 1's current val
-        /// is bigger than list 2's current val, then we assign list 1's val as
-        /// the smallest node.
-        if(list2 == null || (list1 != null && list1.val < list2.val)){
-            smallestNode = list1;
-            list1 = list1.next;
-        } else {
-            smallestNode = list2;
-            list2 = list2.next;
-        }
-        /// First condition is only triggered once.
-        if(resultHead == null){
-            resultHead = smallestNode;
-        } else {
-            /// We're chaning the null value assigned from the previous loop.
-            resultTail.next = smallestNode;
-        }
-        
-        resultTail = smallestNode;
-        resultTail.next = null;
-    }
       
-      return resultHead;
+      ListNode head = null, tail = null;
+      
+      while(list1 != null || list2 != null) {
+          ListNode smallestNode;
+          
+          if(list1 == null || (list2 != null && list2.val < list1.val)) {
+              smallestNode = list2;
+              list2 = list2.next;
+          } else {
+              smallestNode = list1;
+              list1 = list1.next;
+          }
+          
+          if(head == null) {
+              head = smallestNode;
+          } else {
+              tail.next = smallestNode;
+          }
+          
+          tail = smallestNode;
+          tail.next = null;
+          
+      }
+      
+      return head;
   }
 }
