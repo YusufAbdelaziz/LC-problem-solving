92187adfa601b6600cddc454cc90f9aabeeaae8d
Joe <yussefislamanwer@gmail.com>
2023-10-10 01:21:19 +0300

    Time: 0 ms (100.00%), Space: 40.5 MB (55.04%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -9,34 +9,56 @@
  * }
  */
 class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         if(head == null || head.next == null) return null;
+        
+//         // Since head is not null, then we have at least 1 element.
+//         int numOfElements = 0;
+//         ListNode cur = head;
+//         while(cur != null) {
+//             cur = cur.next;
+//             numOfElements++;
+//         }
+        
+//         int removalFromStartIndex = numOfElements - n;
+//         int i = 1;
+//         ListNode newCur = head;
+//         while(i < removalFromStartIndex) {
+//             newCur = newCur.next;
+//             i++;
+//         }
+        
+//         if(removalFromStartIndex == 0) {
+//             return head.next;
+//         }
+        
+//         ListNode next = newCur.next;
+        
+//         newCur.next = newCur.next.next;
+//         next.next = null;
+        
+//         return head;
+//     }
+    
+     public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head == null || head.next == null) return null;
         
-        // Since head is not null, then we have at least 1 element.
-        int numOfElements = 0;
-        ListNode cur = head;
-        while(cur != null) {
-            cur = cur.next;
-            numOfElements++;
+        ListNode dummy = new ListNode(0, head);
+        ListNode l = dummy;
+        ListNode r = head;
+        // Set distance between l and r pointer which is n;
+        while(n > 0) {
+            r = r.next;
+            n--;
         }
         
-        int removalFromStartIndex = numOfElements - n;
-        int i = 1;
-        ListNode newCur = head;
-        while(i < removalFromStartIndex) {
-            newCur = newCur.next;
-            i++;
+        while(r != null) {
+            r = r.next;
+            l = l.next;
         }
-        
-        if(removalFromStartIndex == 0) {
-            return head.next;
-        }
-        
-        ListNode next = newCur.next;
-        
-        newCur.next = newCur.next.next;
-        next.next = null;
-        
-        return head;
+         
+        l.next = l.next.next;
+        return dummy.next;
+         
     }
 }
