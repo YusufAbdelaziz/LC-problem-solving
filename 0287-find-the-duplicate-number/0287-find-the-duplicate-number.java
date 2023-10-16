23a12dbba2be0e97f08e67643c386ac2aae264b3
Joe <yussefislamanwer@gmail.com>
2023-10-16 22:35:48 +0300

    Time: 4 ms (84.51%), Space: 56.1 MB (86.54%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,46 +1,20 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        if(nums == null || nums.length == 0) return 0;
-        // // Time -> O(N).
-        // // Space -> O(N).
-        // Set<Integer> set = new HashSet<>();
-        // int sol = 0;
-        // for(int num : nums) {
-        //     if(set.contains(num)) {
-        //         sol = num;
-        //         return sol;
-        //     }
-        //     set.add(num);
-        // }
-        // return sol;
+        int tortoise = nums[0];
+        int hare = nums[0];
         
-//         // Time -> O(N).
-//         // Space -> O(N).
-//         // Uses an array which is a little bit faster but same space complexity as the first approach.
-//         int[] temp = new int[nums.length];
-//         for(int num : nums) {
-//             temp[num - 1] += 1;
-//         }
+        do {
+            tortoise = nums[nums[tortoise]];
+            hare = nums[hare];
+        } while(tortoise != hare);
         
-//         for(int i = 0; i < nums.length; i++) {
-//             if(temp[i] >= 2) return i + 1;
-//         }
+        tortoise = nums[0];
         
-       int tortoise = nums[0];
-       int hare = nums[0];
-       
-       do {
-           tortoise = nums[tortoise];
-           hare = nums[nums[hare]];
-       } while (tortoise != hare);
-       
-       tortoise = nums[0];
-       
-       while(tortoise != hare) {
-           tortoise = nums[tortoise];
-           hare = nums[hare];
-       }
+        while(tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
         
-       return tortoise;
+        return hare;
     }
 }
