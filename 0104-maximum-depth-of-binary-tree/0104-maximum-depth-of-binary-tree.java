8815373a5434fa63f84f1490a9291ed6c252fee7
Joe <yussefislamanwer@gmail.com>
2023-11-05 22:58:43 +0200

    Time: 1 ms (10.80%), Space: 40.6 MB (99.49%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,11 +15,34 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+        // DFS solution
+        // if(root == null) return 0;
+        
+        // int countLeft = maxDepth(root.left);
+        // int countRight = maxDepth(root.right);
+                
+        // return Math.max(countLeft, countRight) + 1;
+        
+        // BFS solution
+        
         if(root == null) return 0;
         
-        int countLeft = maxDepth(root.left);
-        int countRight = maxDepth(root.right);
+        int level = 0;
         
-        return Math.max(countLeft, countRight) + 1;
+        ArrayDeque<TreeNode> deque = new ArrayDeque<>();
+        deque.add(root);
+        
+        while(!deque.isEmpty()) {
+            int size = deque.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode node = deque.poll();
+                if(node.left != null) deque.add(node.left);
+                if(node.right != null) deque.add(node.right);
+            }
+            
+            level++;
+        }
+        
+        return level;
     }
 }
