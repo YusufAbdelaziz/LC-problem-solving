a84cf75260cdbb0715a1a0f3ec35d7647e53416b
Joe <yussefislamanwer@gmail.com>
2024-01-13 18:36:51 +0200

    Time: 15 ms (53.54%), Space: 45.2 MB (19.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,42 +1,42 @@
-import java.util.HashMap;
-
 class Solution {
     public String minWindow(String s, String t) {
-        int left = 0, right = 0, match = 0, start = 0;
-        int minLen = Integer.MAX_VALUE;
-
+        int right = 0, left = 0, minLen = Integer.MAX_VALUE;
+        
         HashMap<Character, Integer> window = new HashMap<>();
         HashMap<Character, Integer> needs = new HashMap<>();
-
-        for (char c : t.toCharArray()) needs.put(c, needs.getOrDefault(c, 0) + 1);
-
-        while (right < s.length()) {
-            char currentRightChar = s.charAt(right);
-            if (needs.containsKey(currentRightChar)) {
-                window.put(currentRightChar, window.getOrDefault(currentRightChar, 0) + 1);
-                if (window.get(currentRightChar).equals(needs.get(currentRightChar))) {
-                    match++;
-                }
+        
+        int match = 0;
+        int start = 0;
+        
+        for(char c : t.toCharArray()) needs.put(c, needs.getOrDefault(c, 0) + 1);
+        
+        while(right < s.length()) {
+            char rightChar = s.charAt(right);
+            if(needs.containsKey(rightChar)) {
+                window.put(rightChar, window.getOrDefault(rightChar, 0) + 1);
+                if(window.get(rightChar).equals(needs.get(rightChar))) match++;
             }
-
+            
             right++;
-
-            while (match == needs.size()) {
-                if (right - left < minLen) {
+            
+            while(match == needs.size()) {
+                if(right - left < minLen) {
                     start = left;
                     minLen = right - left;
                 }
-                char currentLeftChar = s.charAt(left);
-                if (needs.containsKey(currentLeftChar)) {
-                    window.put(currentLeftChar, window.get(currentLeftChar) - 1);
-                    if (window.get(currentLeftChar) < needs.get(currentLeftChar)) {
-                        match--;
-                    }
+                
+                char leftChar = s.charAt(left);
+                
+                if(needs.containsKey(leftChar)) {
+                    window.put(leftChar, window.getOrDefault(leftChar, 1) - 1);
+                    if(window.get(leftChar) < needs.get(leftChar)) match--;
                 }
+                
                 left++;
             }
         }
-
+        
         return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+
     }
 }
