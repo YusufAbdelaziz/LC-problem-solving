fe6b00234c9e920c71f1eba0e9d311d19dae6b5e
Joe <yussefislamanwer@gmail.com>
2024-01-17 21:15:59 +0200

    Time: 23 ms (32.48%), Space: 44.6 MB (13.01%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -7,18 +7,21 @@ class Solution {
         int left = 0, right = 0;
 
         HashMap<Character, Integer> window = new HashMap<>();
-
+        
+        int maxFreq = 0;
         while (right < s.length()) {
             char rightChar = s.charAt(right);
             window.put(rightChar, window.getOrDefault(rightChar, 0) + 1);
 
-            var maxCharEntry = window.entrySet().stream().max(Map.Entry.comparingByValue()).orElse(null);
-
-            while ((right - left + 1) - maxCharEntry.getValue() > k) {
+            // var maxCharEntry = window.entrySet().stream().max(Map.Entry.comparingByValue()).orElse(null);
+            maxFreq = Math.max(maxFreq, window.get(rightChar));
+            /// window size = right - left + 1
+            while ((right - left + 1) - maxFreq > k) {
                 char leftChar = s.charAt(left);
                 window.put(leftChar, window.get(leftChar) - 1);
                 left++;
-                maxCharEntry = window.entrySet().stream().max(Map.Entry.comparingByValue()).orElse(null);
+                // maxCharEntry = window.entrySet().stream().max(Map.Entry.comparingByValue()).orElse(null);
+                maxFreq = Math.max(maxFreq, window.get(leftChar));
             }
 
             maxLen = Math.max(maxLen, (right - left + 1));
