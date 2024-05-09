f5ef9c9db3e072a25888c6ddafe94122ace93237
Joe <yussefislamanwer@gmail.com>
2024-05-09 23:02:54 +0300

    Time: 4 ms (42.09%), Space: 49.4 MB (46.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,38 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        boolean[][] visitedIslands = new boolean[grid.length][grid[0].length];
+        int numIslands = 0;
+        boolean[][] marked = new boolean[grid.length][grid[0].length];
         
-        int numOfIslands = 0;
-        
-        for(int i = 0 ; i < grid.length ; i ++){
-            for(int j = 0; j < grid[i].length ; j ++){
-                if(!visitedIslands[i][j] && grid[i][j] == '1'){
-                    numOfIslands++;
-                    visitIslandsByDFS(i, j, visitedIslands, grid);
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                if(!marked[i][j] && grid[i][j] != '0') {
+                    numIslands++;
+                    dfs(marked, grid, i, j);
                 }
             }
         }
         
-        return numOfIslands;
+        return numIslands;
     }
     
-    private void visitIslandsByDFS(int i, int j, boolean[][] visitedIslands, char[][] grid){
-        // Check if you've exceeded the limits (horizontally or vertically).
-        if(i < 0 || i >= grid.length ||  j < 0 || j >= grid[i].length )
+    
+    private void dfs(boolean[][] marked, char[][] grid, int i, int j) {
+        if(i >= grid.length || i < 0 || j < 0 || j >= grid[i].length) {
             return;
+        }
         
-        // Check if it's a water or the island was being visited before.
-        if(grid[i][j] != '1' || visitedIslands[i][j])
+        if(grid[i][j] == '0' || marked[i][j]) {
             return;
+        }        
         
         
-        visitedIslands[i][j] = true;
         
-        visitIslandsByDFS(i - 1, j, visitedIslands, grid);
-        visitIslandsByDFS(i + 1, j, visitedIslands, grid);
-        visitIslandsByDFS(i, j - 1, visitedIslands, grid);
-        visitIslandsByDFS(i, j + 1, visitedIslands, grid);
+        marked[i][j] = true;
+        
+        dfs(marked, grid, i + 1, j);
+        dfs(marked, grid, i - 1, j);
+        dfs(marked, grid, i, j + 1);
+        dfs(marked, grid, i, j - 1);
+
     }
 }
