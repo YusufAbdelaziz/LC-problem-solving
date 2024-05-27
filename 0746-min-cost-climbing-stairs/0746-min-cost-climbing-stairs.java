52a5a9977f8d27dee9cdad2651ee14ee7fda5ea4
Joe <yussefislamanwer@gmail.com>
2024-05-28 00:13:44 +0300

    Time: 0 ms (100.00%), Space: 42.8 MB (68.56%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -2,7 +2,7 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
     
        
-        // Top-down approach.
+        // Top-down approach or in-place method.
         // int n = cost.length;
 //         for(int i = 2; i < n; i++) {
 //             cost[i] += Math.min(cost[i - 1], cost[i - 2]);
@@ -10,17 +10,24 @@ class Solution {
         
 //         return Math.min(cost[n - 1], cost[n - 2]);
         
+        // DP array solution.
+//         int n = cost.length;
+//         int[] dp = new int[n + 2];
+//         dp[n] = 0; 
+        
+//         for(int i = n - 1; i >= 0; i--) {
+            
+//                 dp[i] = cost[i] + Math.min(dp[i + 1], dp[i + 2]);
+            
+//         }
+        
+//         return Math.min(dp[0], dp[1]);
+        
         int n = cost.length;
-        int[] dp = new int[n + 2];
-        dp[n] = 0; 
-        
-        for(int i = n - 1; i >= 0; i--) {
-            
-                dp[i] = cost[i] + Math.min(dp[i + 1], dp[i + 2]);
-            
+        for(int i = n - 3; i >= 0; i--) {
+             cost[i] += Math.min(cost[i + 1], cost[i + 2]);
         }
-        
-        return Math.min(dp[0], dp[1]);
+        return Math.min(cost[0], cost[1]);
     }
     
 }
