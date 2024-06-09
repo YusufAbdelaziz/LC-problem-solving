37e17c18927f0063f67ddc64fcc902f724d75563
Joe <yussefislamanwer@gmail.com>
2024-06-09 21:23:35 +0300

    Time: 0 ms (100.00%), Space: 41.2 MB (18.01%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,23 +1,29 @@
 class Solution {
     private int[] memo;
     public int rob(int[] nums) {
-        memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        return dp(nums, 0);
-    }
-    
-    
-    private int dp(int[] nums, int start) {
-        if(start >= nums.length) {
-            return 0;
+        int d1 = 0, d2 = 0;
+        
+        for(int i = nums.length - 1; i >= 0; i--) {
+            int temp = d2;
+            d2 = Math.max(d2, nums[i] + d1);
+            d1 = temp;
         }
-        
-        if(memo[start] != -1) return memo[start];
-        
-        int rob = Math.max(dp(nums, start + 1), nums[start] + dp(nums, start + 2));
-        
-        memo[start] = rob;
-        
-        return rob;
+
+        return d2;
     }
+    
+    // Top-down approach with memo table
+//     private int dp(int[] nums, int start) {
+//         if(start >= nums.length) {
+//             return 0;
+//         }
+        
+//         if(memo[start] != -1) return memo[start];
+        
+//         int rob = Math.max(dp(nums, start + 1), nums[start] + dp(nums, start + 2));
+        
+//         memo[start] = rob;
+        
+//         return rob;
+//     }
 }
