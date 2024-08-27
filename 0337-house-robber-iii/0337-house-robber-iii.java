8550c2aa921f76c3add2bca5dab075aff85e0aff
Joe <yussefislamanwer@gmail.com>
2024-08-28 01:03:46 +0300

    Time: 0 ms (100%), Space: 44.3 MB (71.52%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -1,39 +1,42 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    Map<TreeNode, Integer> memo = new HashMap<>();
-
     public int rob(TreeNode root) {
-        if (root == null)
-            return 0;
+        int[] res = dp(root);
 
-        if (memo.containsKey(root)) {
-            return memo.get(root);
-        }
-
-        int rob = root.val + (root.left == null ? 0 : rob(root.left.left) + rob(root.left.right))
-                + (root.right == null ? 0 : rob(root.right.left) + rob(root.right.right));
-
-        int notRob = rob(root.right) + rob(root.left);
-
-        int res = Math.max(rob, notRob);
-
-        memo.put(root, res);
-
-        return res;        
+        return Math.max(res[0], res[1]);
     }
 
+    // arr[0] -> current node result is not robbed
+    // arr[1] -> current node result is robbed
+    private int[] dp(TreeNode root) {
+        if(root == null) return new int[]{0, 0};
+
+        int[] left = dp(root.left);
+        int[] right = dp(root.right);
+
+        // If you decide to rob the current node, then you have to choose the skipped
+        // left and right value (index 0)?
+        int rob = root.val + left[0] + right[0];
+
+        // If you decide not to rob the current node, you are free to choose
+        // between robbing the left node of skipping it and the same right node follos
+        // the same concept.
+        int noRob = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        return new int[]{noRob, rob};
+    }
 }
