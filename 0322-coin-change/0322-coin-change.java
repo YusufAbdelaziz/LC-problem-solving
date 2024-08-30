8466c8e69b0ce0d04ffae01d91904ad049caf883
Joe <yussefislamanwer@gmail.com>
2024-08-31 00:15:41 +0300

    Time: 14 ms (88.48%), Space: 44.4 MB (59.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,7 +1,7 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if(amount == 0) return 0;
-        
+
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
@@ -16,5 +16,8 @@ class Solution {
             }
         }
         return dp[amount] == amount + 1 ? -1 : dp[amount];
+
+        // Time Complexity -> O(amount * coins)
+        // Space Complexity -> O(amount)
     }
 }
