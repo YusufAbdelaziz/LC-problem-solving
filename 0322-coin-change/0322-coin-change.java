00f1098645891585e26ac64c53ac4d4d0388b7e2
Joe <yussefislamanwer@gmail.com>
2024-08-31 00:14:23 +0300

    Time: 14 ms (88.48%), Space: 44.1 MB (83.15%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(amount == 0) return 0;
+        
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for(int currentAmount = 1; currentAmount < dp.length; currentAmount++) {
+            for(int coin : coins) {
+                // calculate the difference between current amount to be found and
+                // the current coin.
+                if(currentAmount - coin >= 0) {
+                    dp[currentAmount] = Math.min(dp[currentAmount], 1 + dp[currentAmount - coin]);
+                }
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+}
