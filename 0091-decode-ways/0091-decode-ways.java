99d0db8d457afed4e431e5cef55b7c0c76210e96
Joe <yussefislamanwer@gmail.com>
2024-09-29 01:44:55 +0300

    Time: 0 ms (100%), Space: 41.5 MB (76.24%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,17 +1,18 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        int[] memo = new int[n];
+        Integer[] memo = new Integer[n];
         return n == 0 ? 0 : recursivelyDecode(0, s, memo);
     }
 
-    private int recursivelyDecode(int p, String s, int[] memo) {
+    private int recursivelyDecode(int p, String s, Integer[] memo) {
         int n = s.length();
         if(p == n) return 1;
         char c = s.charAt(p);
         if(c == '0') return 0;
+        if(memo[p] != null) return memo[p];
         int res = recursivelyDecode(p + 1, s, memo);
-        if(p < n - 1 && (c == '1' || c == '2') && s.charAt(p + 1) < '7') {
+        if(p < n - 1 && (c == '1' || c == '2' && s.charAt(p + 1) < '7')) {
             res += recursivelyDecode(p + 2, s, memo);
         }
         return memo[p] = res;
