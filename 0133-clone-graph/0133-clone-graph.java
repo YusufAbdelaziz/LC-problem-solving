f955f6a2fe5859ff0062c63d67f8b31daae3fd84
Joe <yussefislamanwer@gmail.com>
2024-11-23 20:34:01 +0200

    Time: 27 ms (37.43%), Space: 42.5 MB (86.93%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,29 +19,34 @@ class Node {
 */
 
 class Solution {
-    private HashMap<Integer, Node> map = new HashMap<>();
-    
+    private Map<Node, Node> map = new HashMap<>();
+
     public Node cloneGraph(Node node) {
-        return clone(node);
-    }
-    
-    
-    private Node clone(Node node) {
-        
-        // Handles empty node case.
-        if(node == null)
-            return null;
-        
-        if(map.containsKey(node.val))
-            return map.get(node.val);
-        
-        Node newNode = new Node(node.val);
-        map.put(node.val, newNode);
-        
-        for(Node neighbor : node.neighbors) {
-            newNode.neighbors.add(clone(neighbor));
-        }
-        
+        if(node == null) return null;
+        if(node.neighbors.size() == 0) return new Node(node.val);
+
+        Node newNode = new Node();
+        copy(newNode, node);
         return newNode;
+    }
+
+    private void copy(Node copyNode, Node originalNode) {
+        
+        copyNode.val = originalNode.val;
+        map.putIfAbsent(originalNode, copyNode);
+        for(Node neighbor : originalNode.neighbors) {
+            if(map.getOrDefault(neighbor, null) != null) {
+                Node copyNeighbor = map.get(neighbor);
+                copyNode.neighbors.add(copyNeighbor);
+
+            } else {
+                Node newNeighborCopy = new Node();
+                copyNode.neighbors.add(newNeighborCopy);
+                copy(newNeighborCopy, neighbor);
+            }
+
+            
+        }
+
     }
 }
