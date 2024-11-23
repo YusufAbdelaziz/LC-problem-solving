1115dc908642eacc5fbe84d0b46ab80ca5569d88
Joe <yussefislamanwer@gmail.com>
2024-11-23 14:38:35 +0200

    Time: 1 ms (100%), Space: 44.9 MB (18.39%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,31 +1,29 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int maxArea = 0;
-        
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j] != 0) {
-                    maxArea = Math.max(maxArea, dfs(i, j, grid));
+        int rowsNum = grid.length;
+        int colsNum = grid[0].length;
+
+        for(int i = 0; i < rowsNum; i++) {
+            for(int j = 0; j < colsNum; j++) {
+                if(grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, calculateIslandArea(grid, i, j));
                 }
             }
         }
-        
+
         return maxArea;
     }
-    
-    
-    private int dfs(int i, int j, int[][] grid) {
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[i].length) {
+
+    private int calculateIslandArea(int[][] grid, int x, int y) {
+        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == 0) {
             return 0;
         }
-        
-        if(grid[i][j] == 0) return 0;
-        
-        grid[i][j] = 0;
-        
-        return 1 + dfs(i + 1, j, grid) +
-               dfs(i - 1, j, grid) +
-               dfs(i, j + 1, grid) +
-               dfs(i, j - 1, grid);
+
+        // Mark the current cell so you don't revisit it again.
+        grid[x][y] = 0;
+
+        return 1 + calculateIslandArea(grid, x + 1, y) + calculateIslandArea(grid, x, y + 1)
+                    + calculateIslandArea(grid, x - 1, y) + calculateIslandArea(grid, x, y - 1);
     }
 }
