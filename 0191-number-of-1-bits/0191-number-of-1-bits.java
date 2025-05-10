a82ef7f84389b712899b7e2aeac05732e3fdcc1b
Joe <yussefislamanwer@gmail.com>
2025-05-10 18:10:36 +0300

    Time: 0 ms (100%), Space: 40.8 MB (41.26%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,5 +1,16 @@
 class Solution {
     public int hammingWeight(int n) {
-        return Integer.bitCount(n);
+        // Brain Kernighan for cleanring set bits.
+
+        int count = 0;
+        
+        while(n != 0) {
+            // ANDing with the preceeding number would result in
+            // setting all bits to 0 on each iteration.
+            n &= (n - 1);
+            count++;
+        }
+
+        return count;
     }
 }
