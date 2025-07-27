acb400ab9c548ea3386ed2f8d1467e76ff5a17a2
Joe <yussefislamanwer@gmail.com>
2025-07-27 23:56:32 +0300

    Time: 9 ms (97.28%), Space: 49.1 MB (25.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,59 +1,37 @@
 class Solution {
-   public int[] topKFrequent(int[] nums, int k) {
-      if (nums.length == 0 || nums == null || nums.length == 1)
-        return nums;
-      // Time -> O(K log n) where K is the number of pops we make for the max heap.
-      // Space -> O(N) where N is the number of pairs that store the number and its
-      // repetitions.
-      // HashMap<Integer, Integer> map = new HashMap<>();
-      // LinkedList<Integer> result = new LinkedList<>();
-      // for (int num : nums) {
-      // map.put(num, map.getOrDefault(num, 0) + 1);
-      // }
+    public int[] topKFrequent(int[] nums, int k) {
+        // Bucket Sort
 
-      // PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b)
-      // -> b.getValue() - a.getValue());
+        int[] result = new int[k];
+        Map<Integer, Integer> occurs = new HashMap<>();
 
-      // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-      // heap.add(entry);
-      // }
-
-      // for (int i = 0; i < k; i++) {
-      // result.add(heap.poll().getKey());
-      // }
-
-      // return result.stream().mapToInt(Integer::intValue).toArray();
-
-      // Solving using bucket sort.
-      // Time -> O(N)
-      // Space -> O(N)
-      HashMap<Integer, Integer> map = new HashMap<>();
-      int[] result = new int[k];
-      for (int num : nums) {
-        map.put(num, map.getOrDefault(num, 0) + 1);
-      }
-
-      ArrayList<Integer>[] count = new ArrayList[nums.length];
-      for (int i = 0; i < count.length; i++) {
-        count[i] = new ArrayList<Integer>();
-      }
-
-      for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-        var valuesArr = count[entry.getValue() - 1];
-        valuesArr.add(entry.getKey());
-      }
-
-      int j = k - 1;
-
-      for (int i = count.length - 1; i >= 0 && j >= 0; i--) {
-        if (count[i].size() > 0) {
-          for (int num : count[i]) {
-            result[j] = num;
-            j--;
-          }
+        for (int num : nums) {
+            occurs.put(num, occurs.getOrDefault(num, 0) + 1);
         }
-      }
 
-      return result;
+        ArrayList<Integer>[] arr = new ArrayList[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        for (Map.Entry<Integer, Integer> entry : occurs.entrySet()) {
+            var curArr = arr[entry.getValue() - 1];
+            curArr.add(entry.getKey());
+        }
+
+        int j = k - 1;
+        for (int i = arr.length - 1; i >= 0 && j  >= 0; i--) {
+            var curArr = arr[i];
+            if (curArr.size() > 0) {
+                for (int e : curArr) {
+                    result[j] = e;
+                    j--;
+                }
+            }
+
+        }
+
+        return result;
     }
 }
