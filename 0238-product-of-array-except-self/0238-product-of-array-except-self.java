f219c9a6f2de44ff9b5266afd33eb65c4c5ccf0b
Joe <yussefislamanwer@gmail.com>
2025-07-31 00:44:10 +0300

    Time: 2 ms (87.47%), Space: 55.8 MB (31.84%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -8,22 +8,22 @@ class Solution {
 
         int[] post = new int[n];
 
-        pre[0] = nums[0];
-        post[n - 1] = nums[n - 1];
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i];
+        pre[0] = 1;
+        post[n - 1] = 1;
+
+        for (int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
         }
 
-        for(int i = n - 2; i > 0; i--) {
-            post[i] = post[i + 1] * nums[i];
+        for (int i = n - 2; i >= 0; i--) {
+            post[i] = post[i + 1] * nums[i + 1];
+
         }
 
-        res[0] = post[1];
-        res[n - 1] = pre[n - 2];
-        for(int i = 1; i < n - 1; i++) {
-            res[i] = post[i + 1] * pre[i - 1]; 
+        for (int i = 0; i < n; i++) {
+            res[i] = pre[i] * post[i];
         }
 
         return res;
     }
-} 
+}
