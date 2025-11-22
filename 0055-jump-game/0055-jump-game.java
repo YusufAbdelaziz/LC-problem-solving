ad81d5acecd4b8e26557e238402f49f1796d1977
Joe <yussefislamanwer@gmail.com>
2025-11-22 18:34:53 +0200

    Time: 2 ms (84.32%), Space: 47.9 MB (10.94%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,36 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        
+        int maxReach = 0;
 
-        int n = nums.length;
-
-        if (n == 0)
-            return false;
-
-        if (n == 1)
-            return true;
-
-        int i = 0;
-        int curSteps = -1;
-        while (i < n - 1 && curSteps != 0) {
-            curSteps = nums[i];
-
-            int bestReach = -1;
-            int maxStepsIndex = i;
-
-            for (int j = 1; j <= curSteps && j + i < n; j++) {
-                int reach = (i + j) + nums[i + j];
-                if (reach > bestReach) {
-                    bestReach = reach;
-                    maxStepsIndex = i + j;
-                }
+        for(int i = 0; i <= maxReach; i++) {
+            int curMaxJump = i + nums[i];
+            if(maxReach < curMaxJump) {
+                maxReach = curMaxJump;
             }
 
-            if (maxStepsIndex == i)
-                return false;
-
-            i = maxStepsIndex;
+            if(maxReach >= nums.length - 1) return true;
         }
 
-        return i >= n - 1;
+
+        return false;
     }
 }
